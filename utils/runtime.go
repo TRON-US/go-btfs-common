d885006c15d9e74894c1e445aca83842579b66f7
uchenna <uchenna.anyanwu@tron.network>
2019-11-11 13:16:51 -0800

    [BTFS-983] - FIXES per jin/weiyu comments

--- utils/runtime.go
+++ utils/runtime.go
@@ -2,109 +2,63 @@ package utils
 
 import (
 	"context"
-	"fmt"
-	"github.com/go-redis/redis/v7"
+
 	sharedpb "github.com/tron-us/go-btfs-common/protos/shared"
+	"github.com/tron-us/go-common/constant"
 	"github.com/tron-us/go-common/db/postgres"
-	dbenv "github.com/tron-us/go-common/env/db"
+	"github.com/tron-us/go-common/db/redis"
+	"github.com/tron-us/go-common/env"
 	"github.com/tron-us/go-common/log"
+
 	"go.uber.org/zap"
-	"os"
-	"strconv"
 )
-
-const (
-	DBConnectionHealthy = "DB Connection Healthy"
-	DBWriteConnectionError = "Cannot connect to the write database!"
-	DBReadConnectionError  = "Cannot connect to the read database!"
-	DBEnvironmentError = "Cannot parse the database environment variable"
-	RedisEnvironmentError = "Cannot parse the redis database environment variable"
-	RedisConnectionError = "Cannot connect to the redis database!"
-)
-
-var (
-	REDISClient *redis.Client
-	RedisHost     = "127.0.0.1"
-	RedisPort     = 6379
-	RedisPassword = ""
-	RedisDb       = 0
-)
-
-func getEnv(key, fallback string) string {
-	value := os.Getenv(key)
-	if len(value) == 0 {
-		return fallback
-	}
-	return value
-}
-
-func ConnectRedis(url string, port int, password string, db int) *redis.Client {
-	var client = redis.NewClient(&redis.Options{
-		Addr:     url + ":" + strconv.Itoa(port),
-		Password: password, // no password set
-		DB:       db,       // use default DB
-	})
-	return client
-}
-
-func CheckRedisConnection(connection *redis.Client) error {
-	val, err := connection.Ping().Result()
-	if err != nil {
-		return err
-	}
-	if val != "PONG" {
-		return fmt.Errorf("Redis respond error %s %d ", RedisHost, RedisPort)
-	}
-	return err
-}
 
 func CheckRuntime(ctx context.Context, runtime *sharedpb.RuntimeInfoRequest) (*sharedpb.RuntimeInfoReport, error) {
 	// db runtime
 	report := new(sharedpb.RuntimeInfoReport)
 
-	// Check postgres dbWrite
-	report.DbStatusExtra = []byte(DBConnectionHealthy)
 	report.Signature = runtime.Signature
 	report.Status = sharedpb.RuntimeInfoReport_RUNNING
 
 	// Check database environment variable
-	dbEnv, err := strconv.ParseBool(getEnv("DB_URL", "false"))
-	if err != nil {
-		log.Error(DBEnvironmentError, zap.Error(err))
-	}
-	if dbEnv == true {
+	_, pgURL := env.GetEnv("DB_URL")
+	if pgURL != "" {
 		// Check postgres dbWrite
-		PGDBWrite := postgres.CreateTGPGDB(dbenv.DBWriteURL)
+		PGDBWrite := postgres.CreateTGPGDB(pgURL)
 		if err := PGDBWrite.Ping(); err != nil {
-			report.DbStatusExtra = []byte(DBWriteConnectionError)
+			report.DbStatusExtra = []byte(constant.DBWriteConnectionError)
 			report.Status = sharedpb.RuntimeInfoReport_SICK
-			log.Error(DBWriteConnectionError, zap.Error(err))
+			log.Error(constant.DBWriteConnectionError, zap.Error(err))
 		}
 		// Check postgres dbRead
-		PGDBRead := postgres.CreateTGPGDB(dbenv.DBReadURL)
+		PGDBRead := postgres.CreateTGPGDB(pgURL)
 		if err := PGDBRead.Ping(); err != nil {
-			report.DbStatusExtra = []byte(DBReadConnectionError)
+			report.DbStatusExtra = []byte(constant.DBReadConnectionError)
 			report.Status = sharedpb.RuntimeInfoReport_SICK
-			log.Error(DBReadConnectionError, zap.Error(err))
+			log.Error(constant.DBReadConnectionError, zap.Error(err))
 		}
+		// Assume the database connection is healthy
+		report.DbStatusExtra = []byte(constant.DBConnectionHealthy)
+	} else {
+		report.DbStatusExtra = nil
 	}
 
 	// Check redis environment variable
-	redisEnv, err := strconv.ParseBool(getEnv("REDIS_URL", "false"))
-	if err != nil {
-		log.Error(RedisEnvironmentError, zap.Error(err))
-	}
-	if redisEnv == true {
-		// Check redis connection
-		error := CheckRedisConnection(ConnectRedis(RedisHost, RedisPort, RedisPassword, RedisDb))
-		if error != nil {
-			report.DbStatusExtra = []byte(RedisConnectionError)
-			log.Error(RedisConnectionError, zap.Error(err))
+	_, redisURL := env.GetEnv("RD_URL")
+	if redisURL != "" {
+		err := redis.CheckRedisConnection(redis.CreateTGRDDB(redisURL))
+		if err != nil {
+			report.RdStatusExtra = []byte(constant.RDConnectionError)
+			report.Status = sharedpb.RuntimeInfoReport_SICK
+			log.Error(constant.RDConnectionError, zap.Error(err))
 		}
+		// Assume the redis connection is healthy
+		report.RdStatusExtra = []byte(constant.RDConnectionHealthy)
+	} else {
+		report.RdStatusExtra = nil
 	}
 
 	// Remaining fields will be populated by the calling service
 	// Reserve: only pass fatal error to higher level
 	return report, nil
 }
-
--- utils/runtime_test.go
+++ utils/runtime_test.go
@@ -2,17 +2,60 @@ package utils
 
 import (
 	"context"
-	"github.com/stretchr/testify/assert"
-	sharedpb "github.com/tron-us/go-btfs-common/protos/shared"
+	"os"
 	"testing"
 	"time"
+
+	sharedpb "github.com/tron-us/go-btfs-common/protos/shared"
+	"github.com/tron-us/go-common/constant"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCheckRuntimeFromGuard(t *testing.T) {
-	ctx , cancel := context.WithTimeout(context.Background(), time.Second)
+func TestCheckRuntimeDBRD(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	shared := new(sharedpb.RuntimeInfoRequest)
-	runtimeInfoReport , _ := CheckRuntime(ctx, shared)
-	assert.Equal(t, runtimeInfoReport.Status, sharedpb.RuntimeInfoReport_RUNNING , "CheckRuntime failed DB status check")
-	assert.Equal(t, runtimeInfoReport.DbStatusExtra, []byte(DBConnectionHealthy), "CheckRuntime failed DB status extra check ")
+	os.Setenv("SS_DB_URL", "postgres://uchenna:Q@vl321!@localhost:5432/db_status")
+	os.Setenv("SS_RD_URL", "redis://uchenna:@127.0.0.1:6379/4?pool=25&process=2")
+	runtimeInfoReport, _ := CheckRuntime(ctx, shared)
+	assert.Equal(t, runtimeInfoReport.Status, sharedpb.RuntimeInfoReport_RUNNING, "CheckRuntime failed DB status check")
+	assert.Equal(t, runtimeInfoReport.DbStatusExtra, []byte(constant.DBConnectionHealthy), "CheckRuntime failed DB status extra check ")
+	assert.Equal(t, runtimeInfoReport.RdStatusExtra, []byte(constant.RDConnectionHealthy), "CheckRuntime failed Redis status extra check ")
+}
+
+func TestCheckRuntimeDBRDFail(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	shared := new(sharedpb.RuntimeInfoRequest)
+	os.Unsetenv("SS_DB_URL")
+	os.Unsetenv("SS_RD_URL")
+	runtimeInfoReport, _ := CheckRuntime(ctx, shared)
+	assert.Equal(t, runtimeInfoReport.Status, sharedpb.RuntimeInfoReport_RUNNING, "CheckRuntime failed DB status check")
+	assert.Equal(t, runtimeInfoReport.DbStatusExtra, []byte(nil), "CheckRuntime did not fail DB status extra check ")
+	assert.Equal(t, runtimeInfoReport.RdStatusExtra, []byte(nil), "CheckRuntime did not fail Redis status extra check ")
+}
+
+func TestCheckRuntimeRDPassDBFail(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	shared := new(sharedpb.RuntimeInfoRequest)
+	os.Setenv("SS_RD_URL", "redis://uchenna:@127.0.0.1:6379/4?pool=25&process=2")
+	os.Unsetenv("SS_DB_URL")
+	runtimeInfoReport, _ := CheckRuntime(ctx, shared)
+	assert.Equal(t, runtimeInfoReport.Status, sharedpb.RuntimeInfoReport_RUNNING, "CheckRuntime failed DB status check")
+	assert.Equal(t, runtimeInfoReport.RdStatusExtra, []byte(constant.RDConnectionHealthy), "CheckRuntime failed Redis status extra check ")
+	assert.Equal(t, runtimeInfoReport.DbStatusExtra, []byte(nil), "CheckRuntime did not fail DB status extra check ")
+}
+
+func TestCheckRuntimeDBPassRDFail(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	shared := new(sharedpb.RuntimeInfoRequest)
+	os.Setenv("SS_DB_URL", "postgres://uchenna:Q@vl321!@localhost:5432/db_status")
+	os.Unsetenv("SS_RD_URL")
+	runtimeInfoReport, _ := CheckRuntime(ctx, shared)
+	assert.Equal(t, runtimeInfoReport.Status, sharedpb.RuntimeInfoReport_RUNNING, "CheckRuntime failed DB status check")
+	assert.Equal(t, runtimeInfoReport.DbStatusExtra, []byte(constant.DBConnectionHealthy), "CheckRuntime failed DB status extra check ")
+	assert.Equal(t, runtimeInfoReport.RdStatusExtra, []byte(nil), "CheckRuntime did not fail Redis status extra check ")
 }
