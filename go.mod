b15ea28bf91f956412b88a899fe663af584f2db2
Eric Chen <eric@tron.network>
2019-11-11 18:04:42 -0800

    Update go-common to v2 and update (soon to be deprecated) Node to use new go-pg v9 "pg:" tags instead of "sql:"

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/gogo/protobuf v1.3.1
 	github.com/libp2p/go-libp2p-core v0.0.6
-	github.com/tron-us/go-common v1.0.1
+	github.com/tron-us/go-common/v2 v2.0.2
 	go.uber.org/zap v1.10.0
 	google.golang.org/grpc v1.24.0
 )
--- info/node.go
+++ info/node.go
@@ -8,39 +8,39 @@ import (
 // TODO: Remove this package after it's fully deprecated.
 
 type Node struct {
-	tableName struct{} `sql:"node_metrics,alias:t" pg:",discard_unknown_columns"`
+	tableName struct{} `pg:"node_metrics,alias:t,discard_unknown_columns"`
 
-	NodeID             string    `sql:"node_id,notnull" json:"node_id"`
-	NodeIP             string    `sql:"node_ip,notnull" json:"node_ip"`
-	CpuInfo            string    `sql:"cpu_info" json:"cpu_info"`
-	BtfsVersion        string    `sql:"btfs_version" json:"btfs_version"`
-	OsType             string    `sql:"os_type" json:"os_type"`
-	ArchType           string    `sql:"arch_type" json:"arch_type"`
-	UpTime             uint64    `sql:"up_time,notnull" json:"up_time"`
-	StorageUsed        uint64    `sql:"storage_used" json:"storage_used"`
-	StorageCap         uint64    `sql:"storage_volume_cap" json:"storage_volume_cap"`
-	MemoryUsed         uint64    `sql:"memory_used" json:"memory_used"`
-	CpuUsed            float64   `sql:"cpu_used" json:"cpu_used"`
-	TimeCreated        time.Time `sql:"time_created" json:"time_created"`
-	Upload             uint64    `sql:"upload" json:"upload"`
-	Download           uint64    `sql:"download" json:"download"`
-	TotalUp            uint64    `sql:"total_upload,notnull" json:"total_upload"`
-	TotalDown          uint64    `sql:"total_download,notnull" json:"total_download"`
-	BlocksUp           uint64    `sql:"blocks_up,notnull" json:"blocks_up"`
-	BlocksDown         uint64    `sql:"blocks_down,notnull" json:"blocks_down"`
-	NumPeers           uint64    `sql:"peers_connected,notnull" json:"peers_connected"`
-	Reputation         float64   `sql:"reputation,notnull" json:"reputation"`
-	StoragePriceDeal   uint64    `sql:"storage_price_deal" json:"storage_price_deal"`
-	BandwidthPriceDeal uint64    `sql:"bandwidth_price_deal" json:"bandwidth_price_deal"`
+	NodeID             string    `pg:"node_id,notnull" json:"node_id"`
+	NodeIP             string    `pg:"node_ip,notnull" json:"node_ip"`
+	CpuInfo            string    `pg:"cpu_info" json:"cpu_info"`
+	BtfsVersion        string    `pg:"btfs_version" json:"btfs_version"`
+	OsType             string    `pg:"os_type" json:"os_type"`
+	ArchType           string    `pg:"arch_type" json:"arch_type"`
+	UpTime             uint64    `pg:"up_time,notnull" json:"up_time"`
+	StorageUsed        uint64    `pg:"storage_used" json:"storage_used"`
+	StorageCap         uint64    `pg:"storage_volume_cap" json:"storage_volume_cap"`
+	MemoryUsed         uint64    `pg:"memory_used" json:"memory_used"`
+	CpuUsed            float64   `pg:"cpu_used" json:"cpu_used"`
+	TimeCreated        time.Time `pg:"time_created" json:"time_created"`
+	Upload             uint64    `pg:"upload" json:"upload"`
+	Download           uint64    `pg:"download" json:"download"`
+	TotalUp            uint64    `pg:"total_upload,notnull" json:"total_upload"`
+	TotalDown          uint64    `pg:"total_download,notnull" json:"total_download"`
+	BlocksUp           uint64    `pg:"blocks_up,notnull" json:"blocks_up"`
+	BlocksDown         uint64    `pg:"blocks_down,notnull" json:"blocks_down"`
+	NumPeers           uint64    `pg:"peers_connected,notnull" json:"peers_connected"`
+	Reputation         float64   `pg:"reputation,notnull" json:"reputation"`
+	StoragePriceDeal   uint64    `pg:"storage_price_deal" json:"storage_price_deal"`
+	BandwidthPriceDeal uint64    `pg:"bandwidth_price_deal" json:"bandwidth_price_deal"`
 
 	NodeStorage
 }
 
 // Host storage publishable information
 type NodeStorage struct {
-	StoragePriceAsk   uint64  `sql:"storage_price_ask" json:"storage_price_ask"`
-	BandwidthPriceAsk uint64  `sql:"bandwidth_price_ask" json:"bandwidth_price_ask"`
-	StorageTimeMin    uint64  `sql:"storage_time_min" json:"storage_time_min"`
-	BandwidthLimit    float64 `sql:"bandwidth_limit" json:"bandwidth_limit"`
-	CollateralStake   uint64  `sql:"collateral_stake" json:"collateral_stake"`
+	StoragePriceAsk   uint64  `pg:"storage_price_ask" json:"storage_price_ask"`
+	BandwidthPriceAsk uint64  `pg:"bandwidth_price_ask" json:"bandwidth_price_ask"`
+	StorageTimeMin    uint64  `pg:"storage_time_min" json:"storage_time_min"`
+	BandwidthLimit    float64 `pg:"bandwidth_limit" json:"bandwidth_limit"`
+	CollateralStake   uint64  `pg:"collateral_stake" json:"collateral_stake"`
 }
