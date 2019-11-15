b7192773143e4ad5890da5966336433b962617ea
Junlin Gao <junlin@tron.network>
2019-11-14 17:44:41 -0800

    Add Const MinQuestionPerChallenge used to prepare the challenge questions

---
+++ config/common.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"github.com/tron-us/go-btfs-common/protos/guard"
+	"strconv"
+
+	"github.com/tron-us/go-common/env"
+	"github.com/tron-us/go-common/log"
+
+	"go.uber.org/zap"
+)
+
+var (
+	ConstMinQuestionsCountPerChallenge = 100
+)
+
+func init() {
+	if _, s := env.GetEnv("CONST_MIN_QUESTIONS_PER_CHALLENGE"); s != "" {
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			log.Panic("Get const_min_questions_per_challenge as string", zap.Error(err))
+		}
+		ConstMinQuestionsCountPerChallenge = v
+	}
+}
+
+func GetMinimumQuestionsCountPerShard(status *guard.FileStoreStatus) (val int) {
+	if status == nil || status.FileStoreMeta.CheckFrequency == 0 {
+		//if it is 0 or status is nil, means using default frequency, 12 per year
+		return ConstMinQuestionsCountPerChallenge * 12
+	}
+
+	return int(status.FileStoreMeta.CheckFrequency) * ConstMinQuestionsCountPerChallenge
+}
