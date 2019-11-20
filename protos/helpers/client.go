3242d51ad6b9451f6d10e0e754c939680e111eed
Robin Lai <laipogo@gmail.com>
2019-11-20 14:45:13 -0800

    feat: new grpc conn

---
+++ protos/helpers/client.go
@@ -0,0 +1,128 @@
+package helpers
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	defaultSchema = "http"
+)
+
+var (
+	domainRegexp = regexp.MustCompile(`^([a-zA-Z0-9-]{1,63}\.)*([a-zA-Z]{1,63})$`)
+	ipv4Regexp   = regexp.MustCompile(`^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$`)
+)
+
+type parsedURL struct {
+	schema string
+	host   string
+	port   int
+}
+
+func NewGRPCConn(ctx context.Context, address string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	u, err := parse(address)
+	if err != nil {
+		return nil, err
+	}
+	if u.schema == "http" {
+		opts = append(opts, grpc.WithInsecure())
+	} else if u.schema == "https" {
+		h2creds := credentials.NewTLS(&tls.Config{NextProtos: []string{"h2"}})
+		opts = append(opts, grpc.WithTransportCredentials(h2creds))
+	} else {
+		return nil, errors.New("not supported schema")
+	}
+	return grpc.DialContext(ctx, fmt.Sprintf("%s:%d", u.host, u.port), opts...)
+}
+
+func parse(rawU string) (*parsedURL, error) {
+	if strings.Index(rawU, "//") == 0 {
+		rawU = defaultSchema + ":" + rawU
+	}
+	if !strings.Contains(rawU, "://") {
+		rawU = defaultSchema + "://" + rawU
+	}
+	u, err := url.Parse(rawU)
+	if err != nil {
+		return nil, err
+	}
+	h, p, err := splitHostPort(u)
+	if err != nil {
+		return nil, err
+	}
+	if err := checkHost(h); err != nil {
+		return nil, err
+	}
+	result := new(parsedURL)
+	result.schema = u.Scheme
+	result.host = h
+	result.port, err = getPort(p, u.Scheme)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func getPort(p string, schema string) (int, error) {
+	if p == "" {
+		switch schema {
+		case "http":
+			p = "80"
+		case "https":
+			p = "443"
+		default:
+			return -1, fmt.Errorf("not support schema: %v", schema)
+		}
+	}
+	return strconv.Atoi(p)
+}
+func checkHost(host string) error {
+	if host == "" {
+		return fmt.Errorf("empty host")
+	}
+
+	host = strings.ToLower(host)
+	if domainRegexp.MatchString(host) {
+		return nil
+	}
+
+	if ipv4Regexp.MatchString(host) {
+		return nil
+	}
+
+	return fmt.Errorf("invalid host")
+}
+
+func splitHostPort(u *url.URL) (host, port string, err error) {
+	if u == nil {
+		return "", "", errors.New("empty url")
+	}
+	host = u.Host
+
+	i := strings.LastIndex(host, ":")
+	if i == -1 {
+		return host, "", nil
+	}
+
+	if i == len(host)-1 {
+		return "", "", errors.New("empty port")
+	}
+
+	port = host[i+1:]
+	host = host[:i]
+
+	if _, err := strconv.Atoi(port); err != nil {
+		return "", "", err
+	}
+
+	return host, port, nil
+}
---
+++ protos/helpers/client_test.go
@@ -0,0 +1,89 @@
+package helpers
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
+	"testing"
+	"time"
+)
+
+func TestNewGRPCConn(t *testing.T) {
+	tests := []struct {
+		in  string
+		out connectivity.State
+		err bool
+	}{
+		{in: "ftp://db-grpc-dev.btfs.io", err: true},
+		{in: "http://db-grpc-dev.btfs.io:443", err: true},
+		{in: "https://db-grpc-dev.btfs.io", out: connectivity.Ready},
+	}
+	for _, tt := range tests {
+		ctx, cancelFunc := context.WithTimeout(context.Background(), 2*time.Second)
+		conn, err := NewGRPCConn(ctx, tt.in, grpc.WithBlock())
+		if cancelFunc != nil {
+			defer cancelFunc()
+		}
+		if !tt.err && err != nil {
+			t.Errorf(`%v: unexpected error \"%v\"`, tt.in, err)
+			continue
+		}
+		if tt.err && err == nil {
+			t.Errorf(`%v: expected error`, tt.in)
+		}
+		if conn != nil {
+			defer conn.Close()
+			state := conn.GetState()
+			if tt.out != state {
+				// don't want to break the test
+				t.Errorf(`%v: got "%v", want "%v"`, tt.in, state, tt.out)
+			}
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		in  string
+		out *parsedURL
+		err bool
+	}{
+		{in: "", err: true},
+		{in: "/", err: true},
+		{in: "//", err: true},
+		{in: "http:/www.google.com", err: true},
+		{in: "http:///www.google.com", err: true},
+		{in: "javascript:void(0)", err: true},
+		{in: "<script>", err: true},
+		{in: "127.0.0:8080", err: true},
+		{in: "127.0.0", err: true},
+		{in: "127.0.0.0.1", err: true},
+		{in: "localhost", out: &parsedURL{schema: "http", host: "localhost", port: 80}},
+		{in: "localhost:8080", out: &parsedURL{schema: "http", host: "localhost", port: 8080}},
+		{in: "btfs.io", out: &parsedURL{schema: "http", host: "btfs.io", port: 80}},
+		{in: "btfs.io:8080", out: &parsedURL{schema: "http", host: "btfs.io", port: 8080}},
+		{in: "127.0.0.1", out: &parsedURL{schema: "http", host: "127.0.0.1", port: 80}},
+		{in: "127.0.0.1:8080", out: &parsedURL{schema: "http", host: "127.0.0.1", port: 8080}},
+		{in: "https://btfs.io", out: &parsedURL{schema: "https", host: "btfs.io", port: 443}},
+		{in: "http://btfs.io", out: &parsedURL{schema: "http", host: "btfs.io", port: 80}},
+		{in: "https://btfs.io:50051", out: &parsedURL{schema: "https", host: "btfs.io", port: 50051}},
+		{in: "http://btfs.io:2333", out: &parsedURL{schema: "http", host: "btfs.io", port: 2333}},
+		{in: "https://127.0.0.1", out: &parsedURL{schema: "https", host: "127.0.0.1", port: 443}},
+		{in: "http://127.0.0.1", out: &parsedURL{schema: "http", host: "127.0.0.1", port: 80}},
+	}
+	for _, tt := range tests {
+		url, err := parse(tt.in)
+		if !tt.err && err != nil {
+			t.Errorf(`%v: unexpected error \"%v\"`, tt.in, err)
+			continue
+		}
+		if tt.err && err == nil {
+			t.Errorf(`%v: expected error`, tt.in)
+		}
+		if tt.out != nil {
+			if tt.out.schema != url.schema || tt.out.host != url.host || tt.out.port != url.port {
+				t.Errorf(`%v: got "%v", want "%v"`, tt.in, url, tt.out)
+			}
+		}
+	}
+}
