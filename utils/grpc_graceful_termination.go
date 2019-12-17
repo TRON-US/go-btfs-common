88c6a0b2eb2e1e68ee9e5c4c836a5a099b3f3773
Ashish Kulkarni <thatInfrastructureGuy@gmail.com>
2019-12-17 11:16:39 -0800

    convert methods to functions
    Signed-off-by: Ashish Kulkarni <thatInfrastructureGuy@gmail.com>

--- utils/grpc_graceful_termination.go
+++ utils/grpc_graceful_termination.go
@@ -4,21 +4,16 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/tron-us/go-common/v2/log"
 	"google.golang.org/grpc"
 )
 
-// ServerCleanupInfo contains information about grpc server
-// Can be used to cleanup the buffers
-type ServerCleanupInfo struct {
-	GrpcServer *grpc.Server
-}
-
 // GracefulTerminateDetect catches SIGTERM and SIGINT
 // This function only detects the signal. It does not do anything with the detect.
 // Use GracefulTerminateExec func to cleanup else write your own after calling GracefulTerminateDetect
-func (grpcInfo ServerCleanupInfo) GracefulTerminateDetect() {
+func GracefulTerminateDetect() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 	defer signal.Stop(interrupt)
@@ -32,9 +27,25 @@ func (grpcInfo ServerCleanupInfo) GracefulTerminateDetect() {
 
 // GracefulTerminateExec performs a default list of tasks to clean up
 // Override this function in your package to do more.
-func (grpcInfo ServerCleanupInfo) GracefulTerminateExec() {
-	if grpcInfo.GrpcServer != nil {
-		log.Info("Shutting down server gracefully")
-		grpcInfo.GrpcServer.GracefulStop()
+func GracefulTerminateExec(grpcServer *grpc.Server) {
+	stopped := make(chan struct{})
+
+	go func() {
+		if grpcServer != nil {
+			log.Info("Shutting down server gracefully")
+			grpcServer.GracefulStop()
+			close(stopped)
+		}
+	}()
+
+	// Set timer for 10 seconds for graceful termination.
+	t := time.NewTimer(10 * time.Second)
+	select {
+	case <-t.C:
+		// Stop if graceful timer exceeds.
+		log.Info("Graceful termination exceeded timeout. Stopping server!")
+		grpcServer.Stop()
+	case <-stopped:
+		t.Stop()
 	}
 }
