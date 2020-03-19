e47ef6d5758f38a5833db16198c9791898006613
Weiyu Xue <wxue2012@gmail.com>
2020-03-19 10:44:08 -0700

    add hub parser grpc client builder

--- utils/grpc/hub.go
+++ utils/grpc/hub.go
@@ -5,6 +5,7 @@ import (
 	hubpb "github.com/tron-us/go-btfs-common/protos/hub"
 )
 
+// hub-query
 func HubQueryClient(addr string) *HubQueryClientBuilder {
 	return &HubQueryClientBuilder{builder(addr)}
 }
@@ -15,5 +16,19 @@ type HubQueryClientBuilder struct {
 
 func (g *HubQueryClientBuilder) WithContext(ctx context.Context, f func(ctx context.Context,
 	client hubpb.HubQueryServiceClient) error) error {
+	return g.doWithContext(ctx, f)
+}
+
+// hub-parser
+func HubParserClient(addr string) *HubParserClientBuilder {
+	return &HubParserClientBuilder{builder(addr)}
+}
+
+type HubParserClientBuilder struct {
+	ClientBuilder
+}
+
+func (g *HubParserClientBuilder) WithContext(ctx context.Context, f func(ctx context.Context,
+	client hubpb.HubParseServiceClient) error) error {
 	return g.doWithContext(ctx, f)
 }
